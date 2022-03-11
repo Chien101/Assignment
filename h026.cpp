57b27e8328be3cfbfa930d0604ad7e946446da04
Chien101 <11030024@st.thsh.tp.edu.tw>
2022-03-11 18:58:40 +0800

    Assignment2 finished

--- h026.cpp
+++ h026.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 using namespace std;
 
-int (int);
+int fn_WinSign(int);
+int fn_GetRes(int,int);
 
 int main(){
     int first=0,i_N=0;
+    bool bool_end=false;
     vector <int> bro,sis;
     cin>>first;
     bro.push_back(first);
@@ -17,13 +19,61 @@ int main(){
         sis.push_back(_in);
     }
 
-    for(int i=0;i<i_N;i++){
-        if(i>=2){
-            if(sis[i-1]==sis[i-2]){
 
+    for(int i=0;i<i_N;i++){
+
+        if(sis[i-1]==sis[i-2] && i>=2){
+                bro.push_back(fn_WinSign(sis[i-2]));
+        }
+        else if(i>0){
+                bro.push_back(sis[i-1]);
+        }
+
+        if(fn_GetRes(bro[i],sis[i])==1){
+            for(vector<int>:: iterator it=bro.begin(); it!=bro.end(); it++){
+                cout<<*it<<" ";
             }
+            cout<<": Won at round "<<i+1<<endl;
+            bool_end=1;
+            break;
+        }
+        else if(fn_GetRes(bro[i],sis[i])==2){
+            for(vector<int>:: iterator it=bro.begin(); it!=bro.end(); it++){
+                cout<<*it<<" ";
+            }
+            cout<<": Lost at round "<<i+1<<endl;
+            bool_end=1;
+            break;
         }
     }
 
-
+    if(!bool_end){
+        for(vector<int>:: iterator it=bro.begin(); it!=bro.end(); it++){
+                cout<<*it<<" ";
+        }
+        cout<<": Drew at round "<<i_N<<endl;
+    }
 }
+
+int fn_WinSign(int _in){
+    if(_in==0) return 5;
+    else if(_in==2) return 0;
+    else return 2;
+}
+
+int fn_GetRes(int p1,int p2){
+    if(p1==p2) return 0;
+    else if(p1==0){
+            if(p2==2) return 1;
+            else return 2;
+    }
+    else if(p1==2){
+        if(p2==5) return 1;
+        else return 2;
+    }
+    else{
+        if(p2==0) return 1;
+        else return 2;
+    }
+}
+
